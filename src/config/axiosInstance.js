44817d5e8239629a340ef83f2640c351d0f55137
yashsaini7060 <yk18052000@gmail.com>
2024-12-16 01:38:04 +0530

    axios configued

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL = "https://localhost:3000/api/v1";
+
+const axiosInstance = axios.create();
+
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.timeout = 2500;
+
+export default axiosInstance;
